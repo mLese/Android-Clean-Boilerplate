a10ac151d42951b6e74c6ae87f01be404b869b77
Michael Lese <michael.lese08@gmail.com>
2016-05-03 14:33:51 -0400

    Adding Test

---
+++ app/src/androidTest/java/com/kodelabs/boilerplate/presentation/ui/activities/MainActivityTest.java
@@ -0,0 +1,30 @@
+package com.kodelabs.boilerplate.presentation.ui.activities;
+
+
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.LargeTest;
+
+import com.kodelabs.boilerplate.R;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class MainActivityTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void testRetrievalMessage() {
+        onView(withId(R.id.welcome_textview)).check(matches(withText("Retrieving")));
+    }
+}
